c7b314b0134c207cd0aae3962189de7b1ec4c1f9
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-05-11 17:53:32 -0500

    markers seem to be rendering woooooohoooooooooo

--- frontend/src/MapRender/index.js
+++ frontend/src/MapRender/index.js
@@ -1,74 +1,50 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 function GoogleMap(props) {
+console.log("these are the props")
+console.log(props)
+  const markers = props.posts.map((post)=>{
+    return (
+      <Marker key={post.id}
+        name={post.date}
+        position={{lat: post.Latitude, lng: post.Longitude}}
+        // icon={{
+          // url: "/pngwave.png",
+          // anchor: new google.maps.Point(32,32),
+          // scaledSize: new google.maps.Size(64,64)
+        // }} 
+      />
+    )
+  })
+  
+  // const makeMarkers = async () => {
+  //   const posts = this.props.posts;
+  //   const google=this.props.google;
+  //    const markersMade = props.posts.map((post) => {
+  //     return (
+  //       <Marker key={post.id}
+  //         name={post.date}
+  //         position={{lat: post.Latitude, lng: post.Longitude}}
+  //         // icon={{
+  //           // url: "/pngwave.png",
+  //           // anchor: new google.maps.Point(32,32),
+  //           // scaledSize: new google.maps.Size(64,64)
+  //         // }} 
+  //       />
+  //     )
+  //   })
+  // }
+
   return (
-    <Map google={props.google} zoom={14} />
+
+    <Map google={props.google} zoom={14}>
+    {markers}
+    </Map>
   )
 }
-export default GoogleApiWrapper({
+export default GoogleApiWrapper(
+(props)=>({
   apiKey: 'AIzaSyB7G8yZAkGYtf2QQzkS1n0E1gZtpPF_h8w',
-  LoadingContainer: GoogleMap
-})(GoogleMap)
+  // LoadingContainer: GoogleMap
+}))(GoogleMap)
 
-
-
-// import React, { Component } from 'react';
-// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-
-// class MapRender extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {}
-//     console.log("these are the map props")
-//   	console.log(props)
-//   }
-
-//   makeMarkers = () => {
-//     const posts = this.props.posts;
-//     const google=this.props.google;
-//     return posts.map((post) => {
-//       return (
-//         <Marker key={post.id}
-//           name={post.date}
-//           position={{lat: post.Latitude, lng: post.Longitude}}
-//           // icon={{
-//             // url: "/pngwave.png",
-//             // anchor: new google.maps.Point(32,32),
-//             // scaledSize: new google.maps.Size(64,64)
-//           // }} 
-//         />
-//       )
-//     })
-//   }
-
-
-//   render() {
-//     const containerStyle = {
-//       position: 'relative',
-//       width: "100%",
-//       height: "100%"
-//     }
-//     const markers = this.makeMarkers();
-//     // const bounds = new this.props.google.maps.LatLngBounds();
-//     // for (let quake in this.props.quakes) {
-//     //   bounds.extend( {lat: quake.corrdinates[1], lng: quake.corrdinates[0]} )
-//     // }
-//     return (
-//       <Map 
-//         containerStyle={containerStyle}
-//         google={this.props.google} 
-//         // bounds={bounds}
-//         initialCenter={{
-//             lat: 42.39,
-//             lng: -72.52
-//         }}
-//       >
-//       {markers}
-//       </Map>
-//     )
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: "AIzaSyB7G8yZAkGYtf2QQzkS1n0E1gZtpPF_h8w"
-// })(MapRender)
