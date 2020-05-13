f659fc1ddd24988fc43e9bc3b7b84966ad246ee8
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-05-12 22:37:04 -0500

    adds common word info to state

--- frontend/src/CommonWordsList/index.js
+++ frontend/src/CommonWordsList/index.js
@@ -22,8 +22,12 @@ export default class CommonWordsList extends Component {
 			})
 			const commonJson = await commonResponse.json()
 			console.log("this is the common json")
-			console.log(commonJson)
-
+			console.log(commonJson.data.common_words)
+			console.log(commonJson.data.posts_with_common_words)
+			this.setState({
+				words: commonJson.data.common_words,
+				posts: commonJson.data.posts_with_common_words
+			})
 
 		}catch(err){
 			console.log(err)
