ed3879027702a18f27f2d3ffbd3be28e9815f373
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-05-12 22:33:02 -0500

    function calls the common words

--- frontend/src/CommonWordsList/index.js
+++ frontend/src/CommonWordsList/index.js
@@ -10,7 +10,27 @@ export default class CommonWordsList extends Component {
 		}
 
 	}
+		async componentDidMount() {
+			await this.getCommonWords()
+		}
+	getCommonWords= async () =>{
+		try{
+			const url = process.env.REACT_APP_API_URL + "/api/v1/posts/common"
+			console.log(url)
+			const commonResponse = await fetch(url, {
+				credentials: "include"
+			})
+			const commonJson = await commonResponse.json()
+			console.log("this is the common json")
+			console.log(commonJson)
 
+
+		}catch(err){
+			console.log(err)
+		}
+
+
+	}
 
 
 
